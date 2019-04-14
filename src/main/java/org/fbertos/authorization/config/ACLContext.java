dec0e967855323b46deb438627fdf22ee0b43733
Fernando Bertos Matilla <fbertos@linalis.com>
2019-04-14 13:53:51 +0200

    Version 1 - Final

--- src/main/java/org/fbertos/authorization/config/ACLContext.java
+++ src/main/java/org/fbertos/authorization/config/ACLContext.java
@@ -4,15 +4,10 @@ import java.net.UnknownHostException;
 
 import org.fbertos.persistence.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.ehcache.EhCacheFactoryBean;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
@@ -32,29 +27,15 @@ import org.springframework.security.acls.mongodb.BasicLookupStrategy;
 import org.springframework.security.acls.mongodb.MongoDBMutableAclService;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import com.mongodb.MongoClient;
-
 @Configuration
 @EnableMongoRepositories(basePackageClasses = {AclRepository.class, UserRepository.class })
 public class ACLContext {
 	@Autowired
-    private Environment environment;
-	
-	@Autowired
 	private AclRepository aclRepository;
 	
-	@Bean
-    public MongoTemplate mongoTemplate() throws UnknownHostException
-    {
-		String host = environment.getProperty("spring.data.mongodb.host");
-		String port = environment.getProperty("spring.data.mongodb.port");
-		String db = environment.getProperty("spring.data.mongodb.database");
-		String user = environment.getProperty("spring.data.mongodb.username");
-		String pwd = environment.getProperty("spring.data.mongodb.password");
-        MongoClient mongoClient = new MongoClient(host, new Integer(port));
-        return new MongoTemplate(mongoClient, db);
-    }
-
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
     @Bean
     public AclAuthorizationStrategy aclAuthorizationStrategy() {
         return new AclAuthorizationStrategyImpl(new SimpleGrantedAuthority("USER_READ"));
@@ -68,7 +49,7 @@ public class ACLContext {
 
     @Bean
     public LookupStrategy lookupStrategy() throws UnknownHostException {
-        return new BasicLookupStrategy(mongoTemplate(), aclCache(), aclAuthorizationStrategy(), permissionGrantingStrategy());
+        return new BasicLookupStrategy(mongoTemplate, aclCache(), aclAuthorizationStrategy(), permissionGrantingStrategy());
     }
 
     @Bean
